ee9ac7db44509665dd61e3d9b8284e9c2f47ac9a
Sean Kerwin <swkerwin@icloud.com>
2021-10-04 11:37:13 +0100

    Added function to write tasks to local storage and tidied up elements

--- app.js
+++ app.js
@@ -4,11 +4,22 @@
 
 const input = document.querySelector(".input");
 const add = document.querySelector(".add");
-const todoList = document.querySelector(".todo-container");
+const todoList = document.querySelector("#todo-container");
 
 /*==========================================*/
 //            *Event listeners*             //
 /*==========================================*/
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  // Your code to run since DOM is loaded and ready
+  if (localStorage.hasOwnProperty('todos')) {
+  const todos = JSON.parse(localStorage.getItem('todos'));
+    todos.forEach(t => {
+      createTodoItem(t)
+    });
+  }
+});
+
 
 add.addEventListener("click", addTodo);
 todoList.addEventListener("click", deleteCheck);
@@ -21,43 +32,113 @@ function addTodo(e) {
   //prevnent form from submitting
   e.preventDefault();
   if (input.value.length > 0) {
-    //creating the taskWrapper
-    const taskWrapper = document.createElement("div");
-    taskWrapper.classList.add("todo");
-
-    //creating the task
-    const task = document.createElement("li");
-
-    task.innerText = input.value;
+ 
+    addTodoToLocalStorage(input.value)
     input.value = "";
-    task.classList.add("tasklist");
-    taskWrapper.appendChild(task);
 
+    renderTodos()
+
+    // reset input value
+    input.value = "";
+  }
+}
+
+function renderTodos() {
+  // remove all the current dom elements as we're going to re-create them
+  // this is inefficient but there's not an easy way to keep localStorage and the DOM in sync
+  // without a framework
+  const tasks = document.getElementById('todo-container');
+  while (tasks.children.length > 0) {
+    var task = tasks.children[0];
+    tasks.removeChild(task);
+  }
+
+  // read todo's from local storage and display them
+  const todos = JSON.parse(localStorage.getItem('todos'));
+  todos.forEach(t => {
+    // check that we're not creating the item again in the dom
+    if (t !== input.value) {
+      createTodoItem(t)
+    }
+  });
+}
+
+function addTodoToLocalStorage(todo) {
+
+  let todos = [];
+
+  // get any current todo's
+  if (localStorage.hasOwnProperty('todos')) {
+    todos = JSON.parse(localStorage.getItem('todos'));
+  }
+  // add new todo to todos array
+  todos.push(todo)
+  // make the array unique (no point in having duplicate todo's)
+  todosUniq = [...new Set(todos)];
+
+  // push new array to local storage
+  localStorage.setItem('todos', JSON.stringify(todosUniq))
+}
+
+function createTodoItem(todo) {
+    // get the container
+    const tasksWrapper = document.getElementById('todo-container');
+    // create the li element
+    const taskItem = document.createElement("li");
+    taskItem.classList.add('task-item')
+
+    // create a div inside the list element
+    const taskInner = document.createElement("div");
+    taskInner.classList.add('task-inner');
+
+    // create a container for the buttons
+    const taskButtons = document.createElement("div");
+    taskButtons.classList.add('buttons');
+
+    // create container for task text
+    const taskText = document.createElement("div");
+    taskText.classList.add('task-text');
+    taskText.innerText = todo;
+
+    // create the buttons
     //completed tasks
     const completedBtn = document.createElement("button");
     completedBtn.innerHTML = `<i class="fa fa-check"></i>`;
     completedBtn.classList.add("completed");
-    taskWrapper.appendChild(completedBtn);
+    taskButtons.appendChild(completedBtn);
 
     //completed tasks
     const trashBtn = document.createElement("button");
     trashBtn.innerHTML = `<i class="fa fa-trash"></i>`;
     trashBtn.classList.add("trash");
-    taskWrapper.appendChild(trashBtn);
-    todoList.appendChild(taskWrapper);
-  }
-}
+    taskButtons.appendChild(trashBtn);
+
+
+    // add the buttons div and text to the tasksInner
+    taskInner.appendChild(taskText);
+    taskInner.appendChild(taskButtons);
+
+    // add the taskInner to the taskItem
+    taskItem.appendChild(taskInner);
+
+    // add the taskItem to the tasksWrapper
+    tasksWrapper.appendChild(taskItem);
+} 
 
 function deleteCheck(e) {
   const item = e.target;
   if (item.classList[0] == "trash") {
-    item.parentElement.classList.add("fall");
-    item.parentElement.addEventListener("transitionend", function () {
-      item.parentElement.remove();
-    });
+    const todos = JSON.parse(localStorage.getItem('todos'));
+    const index = todos.indexOf(item.parentElement.parentElement.innerText)
+    if (index > -1) {
+      todos.splice(index, 1);
+    }
+    localStorage.setItem('todos', JSON.stringify(todos))
+    renderTodos()
   }
   if (item.classList[0] == "completed") {
-    item.previousSibling.style.textDecoration = "line-through";
-    item.parentElement.style.opacity = 0.5;
+    // item.previousSibling.style.textDecoration = "line-through";
+    // item.parentElement.style.opacity = 0.5;
+    console.log('completed');
   }
 }
